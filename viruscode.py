32c12f7c343ed6faf9282e5ae5858d24ea40336c
Johan Ringlander <johan.ringlander@gu.se>
2022-10-02 15:47:19 +0200

    pseudocode

--- viruscode.py
+++ viruscode.py
@@ -1,1 +1,13 @@
-hello
+# Align sequences to ref
+
+# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref for all seqs
+
+# Pair substitution results from each seq with date for each seq in the metadata csv file
+
+# All substitutions divided with number of sequences from that date
+
+# How much substitution on each date
+
+# Make graph of substitution frequency certain dates
+
+
