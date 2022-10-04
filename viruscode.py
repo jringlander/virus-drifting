f73cad7fcb92eae40f7a754ca120d9f9c60dcf7b
Johan Ringlander <johan.ringlander@gu.se>
2022-10-04 16:10:08 +0200

    updates

--- viruscode.py
+++ viruscode.py
@@ -1,13 +1,22 @@
-# Align sequences to ref
+# Input is minimap2 aligned data with sequence metadata in the fasta files. Metadata is formatted and
+#followed by the actual sequence
 
-# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref for all seqs
+# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref for all seqs and put name and number of certain substitutions in new csv file
+fasta=reader
+csv
+from fasta
+if refpos "A" while seqpos ["G""T""C"]
 
-# Pair substitution results from each seq with date for each seq in the metadata csv file
+import pandas as pd
+df.
 
-# All substitutions divided with number of sequences from that date
+# All different types of substitutions individually divided with the number of unique sequences from that date
 
-# How much substitution on each date
+# Substitution frequency of each type of substitutions on each date into csv file
 
-# Make graph of substitution frequency certain dates
+# Make graph of substitution frequency of all possible substitutions for all dates
+matplotlib
+
+
 
 
