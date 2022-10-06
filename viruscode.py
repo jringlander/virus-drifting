8fa23b04aea2ff508566e294b91170137aacfa8e
Johan Ringlander <johan.ringlander@gu.se>
2022-10-06 09:05:59 +0200

    new

--- viruscode.py
+++ viruscode.py
@@ -1,16 +1,26 @@
 # Input is minimap2 aligned data with sequence metadata in the fasta files. Metadata is formatted and
 #followed by the actual sequence
 
-# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref for all seqs and put name and number of certain substitutions in new csv file
-fasta=reader
-csv
-from fasta
-if refpos "A" while seqpos ["G""T""C"]
+import samtools
+import pandas
+import mappy as mp
 
-import pandas as pd
-df.
+a = mp.Aligner("SARS-CoV-2.fa")
 
-# All different types of substitutions individually divided with the number of unique sequences from that date
+
+
+# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref for all seqs and put name and
+# number of certain substitutions in new csv file
+ = int(input())
+
+def subsitution_count()
+    if refpos = "A"
+        and seqpos ["G""T""C"]
+            "A"=1
+
+
+
+# The sum of each different substitutions divided with the number of unique sequences from the respective dates
 
 # Substitution frequency of each type of substitutions on each date into csv file
 
