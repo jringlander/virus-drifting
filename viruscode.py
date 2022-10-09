7f2128364aec8d6718454cff405f6ff9c0d5922c
Johan Ringlander <johan.ringlander@gu.se>
2022-10-09 15:59:59 +0200

    newapproach

--- viruscode.py
+++ viruscode.py
@@ -1,26 +1,46 @@
 # Input is fasta files with sequence metadata in the sequence name and align.
+# import packages
 from Bio import SeqIO
-from Bio.Align import PairwiseAligner
+import subprocess
 
-target = {rec.id: rec.seq for rec in SeqIO.parse('sequence.fasta', "fasta")}
-#print to check target
-print(target)
+# Align sequences
+subprocess.run('cat sequence.fasta gisaid_hcov-19_2022_10_04_14.fasta > ref_query.fasta| mafft ref_query.fasta > aligned.fasta', shell=True)
 
-queries = {rec.id: rec.seq for rec in SeqIO.parse('gisaid_hcov-19_2022_10_04_14.fasta', "fasta")}
-print(queries)
+# List and dictionary of alignment
+multifasta = list(SeqIO.parse("aligned.fasta", format = 'fasta'))
 
-for k,v in queries.items():
-    # align v to target
-    aligner = PairwiseAligner()
-    aligner.gap_score = -10
-    alignments = aligner.align(target, queries)
-    len(alignments)
-    alignment = alignments[0]
-    print(alignment)
+for entry in multifasta:
+    print(entry.id)
+
+queries = {}
+for entry in multifasta:
+    queries[entry.id] = entry
+
+print(queries['hCoV-19/Poland/WSSEGorzow-22S3280/2022|EPI_ISL_15234788|2022-09-16'].seq)
+
+for entry in multifasta:
+    print(len(entry.seq))
+
+# Define gaps
+def gapped_pos(seq, pos):
+    no_gap = 0
+    gaps = 0
+    for nt in seq:
+        if nt !='-':
+            no_gap += 1
+        else:
+            gaps += 1
+            if no_gap == pos:
+                return pos + gaps
 
 # Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref
-m = alignment.substitutions
-print(m)
+def get_mutations(initial, variant):
+    queries = list(zip(initial, variant))
+    for pos, nt in enumerate(queries):
+        if nt[0] != nt[1]:
+            print(nt[0].upper() + str(pos) + nt[1].upper())
+
+get_mutations(queries['NC_045512.2'], queries['hCoV-19/Poland/WSSEGorzow-22S3285/2022|EPI_ISL_15234790|2022-09-20'])
 
 # The sum of each different substitutions divided with the number of unique sequences from the respective dates
 
