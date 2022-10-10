ec31f7201ec993b01adf6b1a8287af1c2e7932ac
Johan Ringlander <johan.ringlander@gu.se>
2022-10-10 07:34:09 +0200

    update

--- viruscode.py
+++ viruscode.py
@@ -2,24 +2,18 @@
 # import packages
 from Bio import SeqIO
 import subprocess
+import matplotlib.pyplot as plt
+
 
 # Align sequences
-subprocess.run('cat sequence.fasta gisaid_hcov-19_2022_10_04_14.fasta > ref_query.fasta| mafft ref_query.fasta > aligned.fasta', shell=True)
+subprocess.run('cat sequence.fasta gisaid_hcov-19_2022_10_04_14.fasta > ref_query.fasta | mafft ref_query.fasta > aligned.fasta', shell=True)
 
 # List and dictionary of alignment
 multifasta = list(SeqIO.parse("aligned.fasta", format = 'fasta'))
 
-for entry in multifasta:
-    print(entry.id)
-
 queries = {}
 for entry in multifasta:
     queries[entry.id] = entry
-
-print(queries['hCoV-19/Poland/WSSEGorzow-22S3280/2022|EPI_ISL_15234788|2022-09-16'].seq)
-
-for entry in multifasta:
-    print(len(entry.seq))
 
 # Define gaps
 def gapped_pos(seq, pos):
@@ -33,14 +27,22 @@ def gapped_pos(seq, pos):
             if no_gap == pos:
                 return pos + gaps
 
-# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref
+# Call mutations compared to reference
 def get_mutations(initial, variant):
+    out = []
     queries = list(zip(initial, variant))
     for pos, nt in enumerate(queries):
         if nt[0] != nt[1]:
-            print(nt[0].upper() + str(pos) + nt[1].upper())
+            out.append(nt[0].upper() + str(pos) + nt[1].upper())
+    return out
 
-get_mutations(queries['NC_045512.2'], queries['hCoV-19/Poland/WSSEGorzow-22S3285/2022|EPI_ISL_15234790|2022-09-20'])
+# Print mutations compared to reference
+for item in queries:
+    print (item + ' '+str(len(get_mutations(queries['NC_045512.2'], queries[item]))))
+
+# Count all substitutions (AT, AC, AG, GC, GA, GT, TA, TC, TC, CA, CG, CT) compared to ref
+for item in queries:
+
 
 # The sum of each different substitutions divided with the number of unique sequences from the respective dates
 
@@ -48,6 +50,9 @@ get_mutations(queries['NC_045512.2'], queries['hCoV-19/Poland/WSSEGorzow-22S3285
 
 # Make graph of substitution frequency of all possible substitutions for all dates
 # matplotlib
+#for y, item in enumerate(queries):
+    #plt.plot((0, len(queries['NC_045512.2'])), (y,y), color = 'lightgrey')
+
 
 
 
